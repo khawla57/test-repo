39899611496ce9c2cffcb499dff22a5b829c89bb
khawla57 <khawlaboughanmi77@gmail.com>
2024-06-30 16:59:49 +0100

    deploy to app service

--- src/main/java/esprit/CatchTalent/candidatservice/controller/AuthController.java
+++ src/main/java/esprit/CatchTalent/candidatservice/controller/AuthController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600 , allowCredentials="true")
+@CrossOrigin(origins = "https://catchtalent-app.azurewebsites.net", maxAge = 3600 , allowCredentials="true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
